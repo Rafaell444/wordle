117b0115d49f7a3f5bce49e7e5075838cdffaeab
rafaell444 <raphaeli.mkhitarian.1@btu.edu.ge>
2022-02-10 11:01:42 +0400

    checking if word exists ini db

--- main.py
+++ main.py
@@ -29,6 +29,9 @@ with open("Ka2.txt", "r") as f:
 word_to_guess = random.choice(words)
 # print(word_to_guess)
 
+grey = []
+yellow = []
+green = []
 
 temp = 0
 while temp != 6:
@@ -36,60 +39,76 @@ while temp != 6:
     temp += 1
 
     fake = True
-
-    if len(inp) == 5:
-        if inp != word_to_guess:
-            sia1 = []
-
-
-            def is_grey():
-
-                already_sorted = list(''.join(set(word_to_guess)))
-                for letter in inp:
-                    if letter not in already_sorted:
-                        sia1.append(letter)
-                sd = list(dict.fromkeys(sia1))
-                for one in sd:
-                    print(one, "is grey")
+    if inp in wholetext:
+        if len(inp) == 5:
+            if inp != word_to_guess:
+                sia1 = []
 
 
-            is_grey()
+                def is_grey():
+
+                    already_sorted = list(''.join(set(word_to_guess)))
+                    for letter in inp:
+                        if letter not in already_sorted:
+                            sia1.append(letter)
+                    sd = list(dict.fromkeys(sia1))
+                    for one in sd:
+                        grey.append(one)
+                        print(one, "is grey")
 
 
-            def is_green():
-                for num in range(len(inp)):
-                    if inp[num] == word_to_guess[num]:
-                        print(inp[num], "is green")
+                is_grey()
 
 
-            is_green()
+                def is_green():
+                    for num in range(len(inp)):
+                        if inp[num] == word_to_guess[num]:
+                            green.append(inp[num])
+                            print(inp[num], "is green")
 
 
-            def is_yellow():
-                sia = []
-                for letter in inp:
-                    if letter in word_to_guess:
-                        for num in range(len(inp)):
-                            if inp[num] != word_to_guess[num]:
-                                sia.append(inp[num])
-                without_dublicates = list(dict.fromkeys(sia))
-                for each in without_dublicates:
-                    if each not in sia1:
-                        print(each, "is Yellow")
+                is_green()
 
 
-            is_yellow()
+                def is_yellow():
+                    sia = []
+                    for letter in inp:
+                        if letter in word_to_guess:
+                            for num in range(len(inp)):
+                                if inp[num] != word_to_guess[num]:
+                                    sia.append(inp[num])
+                    without_dublicates = list(dict.fromkeys(sia))
+                    for each in without_dublicates:
+                        if each not in sia1:
+                            yellow.append(each)
+                            print(each, "is Yellow")
+
+
+                is_yellow()
+
+            else:
+                fake = False
+                temp = 6
+                print(f"სწორია!,გამოსაცნობი სიტყვა იყო: {word_to_guess}")
 
         else:
             fake = False
+            fake_len = len(inp)
             temp = 6
-            print(f"სწორია!,გამოსაცნობი სიტყვა იყო: {word_to_guess}")
+            print("სიტყვა უნდა შედგებოდეს 5 ასოსგან,თქვენი სიტყვა კი შედგება {} ასოსგან!".format(fake_len))
+
+        if temp == 6 and fake is True:
+            print("სამწუხაროდ თქვენ წააგეთ")
+            print("ჩაფიქრებული სიტყვა იყო - {}".format(word_to_guess))
 
     else:
-        fake = False
-        fake_len = len(inp)
-        temp = 6
-        print("სიტყვა უნდა შედგებოდეს 5 ასოსგან,თქვენი სიტყვა კი შედგება {} ასოსგან!".format(fake_len))
+        print(f"{inp} - ასეთი სიტყვა არ არსებობს ან არ არის ბაზაში,გთხოვთ ცადოთ სხვა !", )
+        temp += 1
 
-    if temp == 6 and fake is True:
-        print("სამწუხაროდ თქვენ წააგეთ")
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
